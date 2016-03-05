0e526633913224795e6889a4e2221c74b3059d6a
Mateusz Lewko <mateusz.lewko@gmail.com>
2016-03-05 12:26:33 +0100

    improved - mRSE below 0.1, code cleanup

--- solution_LinearRegression.py
+++ solution_LinearRegression.py
@@ -11,18 +11,36 @@ X_CNT = 24
 
 data = []
 
+# parse data #
 with open(FILE_NAME) as file:
     for row in file:
         data.append([float(val) for val in row.split(',')[1:]])
         
 shuffle(data)
 TRAIN_CNT = int(.9 * len(data))
-        
+print 'train cnt:', TRAIN_CNT
+
 Y = [item[LAST] for item in data]
 Y_log = map(lambda x: log(x), Y)
 X = []
- 
+
+X_lr = []
+X_lr_log = []
+
+# compute X_lr and X_lr_log #
+for item in data:
+    regr = linear_model.LinearRegression()
+    regr.fit([[val] for val in range(1, X_CNT + 1)], item[:X_CNT])
+    
+    X_lr.append(regr.coef_[0])
+    X_lr_log.append(log(regr.coef_[0]))
+    
 #print Y_log
+
+# train LinearRegression model #
+train_regr = linear_model.LinearRegression()
+train_regr.fit([[val] for val in X_lr_log[:TRAIN_CNT]], Y_log[:TRAIN_CNT])
+
 
 def plot_distribution():
     """Distribution for diferrent days and log transformed
@@ -38,36 +56,44 @@ def plot_distribution():
         plot.title('Log(x), up to day: ' + str(day))
         plot.show()
     
-X_lr = []
-for item in data:
-    regr = linear_model.LinearRegression()
-    regr.fit([[val] for val in range(1, X_CNT + 1)], item[:X_CNT])
+
+def plot_LinearRegression():
+    """ plot LinearRegression coeff of first 24 hours and Output (Y - original)
+    """
+    plot.scatter(X_lr, Y)
+    plot.show()
+
+    # log transformed
+    plot.title('X - normal, Y - log')
+    plot.scatter(X_lr, Y_log)
+    plot.show()
+
+    # both log transformed
+    plot.title('X - log, Y - log')
+    plot.scatter(X_lr_log, Y_log)
+    plot.show()
+
+
+    train_regr = linear_model.LinearRegression()
+    train_regr.fit([[val] for val in X_lr_log[:TRAIN_CNT]], Y_log[:TRAIN_CNT])
+
+def predict():
+    Y_predicted = []
+    error_sum = 0
+
+    for coeff, real in zip(X_lr_log[TRAIN_CNT:], Y[TRAIN_CNT:]):
+        pred = e ** train_regr.predict(coeff)[0]
+        
+        print 'real:', '%9s' % str(int(real)), '| predicted:', int(pred)
+        
+        error_sum += abs(float(pred) / real - 1.0) ** 2
+        Y_predicted.append(pred)
+        
+    print '\nmRSE:', error_sum / float(len(Y_predicted))
     
-    X_lr.append(regr.coef_[0])
     
-# plot LinearRegression coeff of first 24 hours and Output (Y - original)
-plot.scatter(X_lr, Y)
-plot.show()
-
-# log transformed
-plot.scatter(X_lr, Y_log)
-plot.show()
-
-print 'train cnt:', TRAIN_CNT
-
-train_regr = linear_model.LinearRegression()
-train_regr.fit([[val] for val in X_lr[:TRAIN_CNT]], Y[:TRAIN_CNT])
-
-Y_predicted = []
-error_sum = 0
-
-for coeff, real in zip(X_lr[TRAIN_CNT:], Y[TRAIN_CNT:]):
-    pred = train_regr.predict(coeff)
-    print 'real:', real, '| predicted:', pred[0]
+if __name__ == '__main__':
+    plot_distribution()
+    plot_LinearRegression()
     
-    error_sum += (float(pred) / real - 1.0) * (float(pred) / real - 1.0)
-    Y_predicted.append(pred)
-    
-# something wron with skl msr
-print 'error?:', mean_squared_error(Y[TRAIN_CNT:], Y_predicted)
-print 'mRSE:', error_sum / float(len(Y_predicted))
+    predict()
